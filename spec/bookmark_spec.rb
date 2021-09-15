f3f260a90fab104ac0c1916988a2c0d60ff9b632
Florence Bain <florence_bain@outlook.com>
2021-09-15 15:17:41 +0100

    setting up bookmark test

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,6 +4,13 @@ describe Bookmark do
   let(:bookmark) { Bookmark.all}
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+ 
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+
+      bookmarks = Bookmark.all
 
       expect(bookmark).to include("http://www.makersacademy.com")
       expect(bookmark).to include("http://www.google.com")
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -7,10 +7,16 @@ end
 
 feature 'Viewing bookmarks' do
   scenario 'User can see all bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES (2, 'http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks VALUES (3, 'http://www.destroyallsoftware.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.google.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content"http://www.destroyallsoftware.com"
   end
 end
