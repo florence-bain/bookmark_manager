3bea0869c27ec61f2817ad114fbee1c9ba4ebafa
Florence Bain <florence_bain@outlook.com>
2021-09-17 09:48:04 +0100

    wrap database data in program objects

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,27 +3,42 @@ require 'dotenv'
 
 class Bookmark
 
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end 
+
  Dotenv.load
 
   puts ENV['DBNAME']
 
   def self.all()
-    # if ENV['ENVIRONMENT'] == 'test'
-    #   connection = PG.connect(dbname: 'bookmark_manager_test')
-    # else 
-    #   connection = PG.connect(dbname: 'bookmark_manager')
-    # end 
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else 
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end 
 
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark['url']}
+      result = connection.exec("SELECT * FROM bookmarks;")
+      result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+      end 
+    end
+
+  def self.create(url:, title:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+      result = connection.exec("INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, title, url;")
+      Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+
   end
 
-  def self.create(url:)
-
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('#{url}')")
-  end
 
   # def self.connect
   #   begin
--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -2,8 +2,9 @@ feature 'Adding a new bookmark' do
     scenario 'A user can add a bookmark' do
         visit('/bookmarks/new')
         fill_in('url', with: 'http//example.org')
+        fill_in('title', with: 'Test Bookmark')
         click_button('Submit')
-
-        expect(page).to have_content('http//example.org')
+      
+        expect(page).to have_link("Test Bookmark", href: "http//example.org")
     end
 end
